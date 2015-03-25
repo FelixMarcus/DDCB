aabb11c1d107ada799e91dac702e2a96ba756f47
FelixMarcus <felix.millne@btinternet.com>
2015-03-25 17:09:37 +0000

    Expand character to include simple levelling and string verification

--- src/CharacterTest.java
+++ src/CharacterTest.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 
 
@@ -23,5 +25,46 @@ public class CharacterTest {
 		assertEquals("New Character", character.name());
 		character.rename("Renamed");
 		assertEquals("Renamed", character.name());
+	}
+	
+	@Test
+	public void createBlankNameTest(){
+		DDCharacter character = new DDCharacter();
+		assertEquals("New Character", character.name());
+		character.rename("");
+		assertEquals("Default Character Name", character.name());
+		character.rename(null);
+		assertEquals("Default Character Name", character.name());
+	}
+	
+	@Test 
+	public void characterGetFirstLevelTest(){
+		DDCharacter character = new DDCharacter();
+		assertEquals(0, character.level());
+	}
+	
+	@Test 
+	public void characterLevelUpTest(){
+		DDCharacter character = new DDCharacter();
+		character.levelUp("Fighter");
+		assertEquals(1, character.level());
+		assertEquals(Arrays.asList("Fighter"), character.characterClass());
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void characterLevelUpBlankClassTest(){
+		DDCharacter character = new DDCharacter();
+		character.levelUp("");
+	}
+	
+	@Test 
+	public void characterLevelUpTwoClassTest(){
+		DDCharacter character = new DDCharacter();
+		character.levelUp("Fighter");
+		assertEquals(1, character.level());
+		assertEquals(Arrays.asList("Fighter"), character.characterClass());
+		character.levelUp("Cleric");
+		assertEquals(2, character.level());
+		assertEquals(Arrays.asList("Fighter", "Cleric"), character.characterClass());
 	}
 }
--- src/DDCharacter.java
+++ src/DDCharacter.java
@@ -1,15 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class DDCharacter {
 
-	String name;
-	
+	private String name;
+	private int level;
+	private ArrayList<String> charClass  = new ArrayList<>();
+
 	public DDCharacter(String newName) {
 		this();
 		rename(newName);
 	}
-	
+
 	public DDCharacter() {
 		name = "New Character";
+		level = 0;
 	}
 
 
@@ -18,7 +24,29 @@ public class DDCharacter {
 	}
 
 	public void rename(String newName) {
+		if(newName == "" || newName == null){
+			newName = "Default Character Name";
+		}
+				
 		name = newName;
 	}
+
+	public int level() {
+		return level;
+	}
+
+	public void levelUp(String inClass) {
+		if(inClass.isEmpty() || inClass == null)
+			throw new IllegalArgumentException("Blank class name passed on level up. Level up not continued.");
+		
+		level++;
+		charClass.add(inClass);
+	}
+
+	public List characterClass() {
+		return charClass;
+	}
+
+
 
 }
