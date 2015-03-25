75634be0838276770a15c060c83baf3f79a6f195
FelixMarcus <felix.millne@btinternet.com>
2015-03-25 17:46:56 +0000

    Add new and save functions to controller

--- src/controller/CharacterController.java
+++ src/controller/CharacterController.java
@@ -1,5 +1,19 @@
 package controller;
 
+import characterData.DDCharacter;
+import printers.CharacterXMLPrinter;
+
 public class CharacterController {
 
+	private DDCharacter character;
+
+	public void newCharacter() {
+		character = new DDCharacter();
+	}
+
+	public String save(CharacterXMLPrinter characterXMLPrinter) {
+		return characterXMLPrinter.print(character);
+	}
+	
+	
 }
--- src/controller/CharacterControllerTests.java
+++ src/controller/CharacterControllerTests.java
@@ -1,5 +1,22 @@
 package controller;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import characterData.DDCharacter;
+import printers.CharacterXMLPrinter;
+
 public class CharacterControllerTests {
 
+	@Test 
+	public void saveNewCharacterTest(){
+		CharacterController controller = new CharacterController();
+		controller.newCharacter();
+		String saveString = controller.save(new CharacterXMLPrinter());
+		
+		CharacterXMLPrinter printer = new CharacterXMLPrinter();
+		String controlString = printer.print(new DDCharacter());
+		assertEquals(saveString, controlString);
+	}
 }
